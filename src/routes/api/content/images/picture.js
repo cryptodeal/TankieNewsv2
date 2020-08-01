dc4067331f63e8e2f05045fddd50681076facad2
James Deal <jimmydeal16@gmail.com>
2020-08-01 13:55:55 -0500

    sharp compress and convert images, upload all to S3

--- src/routes/api/content/images/picture.js
+++ src/routes/api/content/images/picture.js
@@ -3,19 +3,78 @@ const AWS = require('aws-sdk');
 const sharp = require('sharp');
 
 export async function post(req, res){
+  let sizes = [400, 800, 1200]
   if (!req.files || Object.keys(req.files).length === 0) {
     return (res.statusCode=400,res.end('No files were uploaded.'));
   }
 
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   const file = req.files.sampleFile;
+  const baseName = file.name.substring(0, file.name.lastIndexOf('.'));
   if(!file.mimetype.startsWith('image')) {
     return (res.statusCode=400,res.end('Images only!'));
   }
 
-  sharp(file.data)
-  //.resize(200)
-  .toFormat('webp')
+  const promises = []
+  sizes.map(size => {
+    promises.push(webpEditUpload(size, file, baseName))
+    promises.push(jpegEditUpload(size, file, baseName))
+  })
+
+  return Promise.all(promises).then(data => {
+    data.map(datum => console.log(datum.Location))
+  })
+
+  // Use the mv() method to place the file somewhere on your server
+//  sampleFile.mv(`uploads/${sampleFile.name}`, function(err) {
+//    if (err)
+//      return (res.statusCode=500,res.end(JSON.stringify(err)))
+//  });
+  //editImage(saved)
+}
+
+function jpegEditUpload(size, image, baseName){
+  let jpegOptions = {
+    quality: 70,
+    progressive: false,
+    force: false
+  }
+  return sharp(image.data)
+    .resize({width: size, withoutEnlargement: true})
+    .jpeg(jpegOptions)
+    .toBuffer()
+    .then(data => { 
+      const S3_Bucket = `dev-tankienews`
+
+      AWS.config.update({
+        accessKeyId: process.env.AWS_ID,
+        secretAccessKey: process.env.AWS_SECRET
+      })
+      const s3 = new AWS.S3();
+
+      let params = {
+        Bucket: S3_Bucket,
+        Key: `${baseName}-${size}.jpeg`,
+        Body: data
+      }
+
+      let s3Promise = s3.upload(params).promise()
+      return s3Promise
+    })
+    .catch( err => { 
+      console.log(err)
+      return err
+    });
+}
+function webpEditUpload(size, image, baseName){
+  let webpOptions = {
+    quality: 75,
+    lossless: false,
+    force: true
+  }
+  return sharp(image.data)
+  .resize({width: size, withoutEnlargement: true})
+  .webp(webpOptions)
   .toBuffer()
   .then(data => { 
     const S3_Bucket = `dev-tankienews`
@@ -24,38 +83,19 @@ export async function post(req, res){
       accessKeyId: process.env.AWS_ID,
       secretAccessKey: process.env.AWS_SECRET
     })
-    file.name = file.name.substring(0, file.name.lastIndexOf('.'));
-    console.log(file.name)
     const s3 = new AWS.S3();
 
     let params = {
       Bucket: S3_Bucket,
-      Key: `${file.name}.webp`,
+      Key: `${baseName}-${size}.webp`,
       Body: data
     }
 
-    s3.upload(params, function(err, data) {
-      if (err) {
-        console.log(err)
-        return (res.statusCode=500,res.end(JSON.stringify(err)))
-      }
-      console.log(data)
-      return (res.end(JSON.stringify(data)))
-    })
+    let s3Promise = s3.upload(params).promise()
+    return s3Promise
   })
   .catch( err => { 
     console.log(err)
-    return (res.statusCode=500,res.end(JSON.stringify(err)))
+    return err;
    });
-  
-  // Use the mv() method to place the file somewhere on your server
-//  sampleFile.mv(`uploads/${sampleFile.name}`, function(err) {
-//    if (err)
-//      return (res.statusCode=500,res.end(JSON.stringify(err)))
-//  });
-  //editImage(saved)
-}
-function editImage(sampleFile){
-
-
 }
